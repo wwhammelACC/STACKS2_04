ae3fb372e15f8527b02c4de238fa91ad5144bf44
wwhammelACC <89032402+wwhammelACC@users.noreply.github.com>
2022-05-25 10:49:27 -0500

    added header

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
-/*
- * your header here
- * 
- */
+/*********************
+Name: William Hammel
+Coding 04: Stacks Practice
+Purpose: : In this assignment you will
+create a stack object that will work with
+a struct data type called Data. The struct
+data type contains an int called id and a
+string called information.
+**********************/
 
 #include "main.h"
 
