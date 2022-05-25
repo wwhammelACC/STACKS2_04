7cb99c507b50361f5c08afc16cfb8d4d9d5ad291
wwhammelACC <89032402+wwhammelACC@users.noreply.github.com>
2022-05-25 10:49:08 -0500

    added header

--- functions.h
+++ functions.h
@@ -1,7 +1,12 @@
-/*
- * Your comment header here
- * modify this file as needed to complete the assignment
- */
+/*********************
+Name: William Hammel
+Coding 04: Stacks Practice
+Purpose: : In this assignment you will
+create a stack object that will work with
+a struct data type called Data. The struct
+data type contains an int called id and a
+string called information.
+**********************/
 
 #ifndef STACKS_FUNCTIONS_H
 #define STACKS_FUNCTIONS_H
